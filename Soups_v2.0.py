a2f6883d9eca1263e1b43c2a812d0ea560031949
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-17 23:55:00 -0400

    Update Soups_v2.0.py

--- Soups_v2.0.py
+++ Soups_v2.0.py
@@ -1,5 +1,11 @@
 import discord
+from discord.ext import commands
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '#')
+
+
+@client.event
+async def on_ready():
+    print('Soups_v2.0 is ready!')
 
 client.run('NzMzODQ4OTI5MDUzMTgwMDE1.XxJQcQ.03F4iDX4CW1AUgXaU_8E3RIQ-Oo')

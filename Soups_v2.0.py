07bfe1ab6405ad477a12004533bc2e5657ccad7e
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-18 00:38:42 -0400

    Update Soups_v2.0.py

--- Soups_v2.0.py
+++ Soups_v2.0.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '#')
@@ -7,6 +8,7 @@ client = commands.Bot(command_prefix = '#')
 async def on_ready():
     print('Soups_v2.0 is ready!')
 
+# Don't need or want this
 """@client.event
 async def on_member_join(member):
     print(f'{member} has joined a server.')
@@ -14,5 +16,48 @@ async def on_member_join(member):
 @client.event
 async def on_member_remove(member):
     print(f'{member} has left a server.')"""
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+@client.command()
+async def youtube(ctx):
+    await ctx.send('```https://www.youtube.com/channel/UC0KW9Y85cFkrZyPkWrNVRUQ```')
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = ['As I see it, yes.',
+                 'Ask again later.',
+                 'Better not tell you now.',
+                 'Cannot predict now.',
+                 'Concentrate and ask again.',
+                 'Don’t count on it.',
+                 'It is certain.',
+                 'It is decidedly so.',
+                 'Most likely.',
+                 'My reply is no.',
+                 'My sources say no.',
+                 'Outlook not so good.',
+                 'Outlook good.',
+                 'Reply hazy, try again.',
+                 'Signs point to yes.',
+                 'Very doubtful.',
+                 'Without a doubt.',
+                 'Yes.',
+                 'Yes – definitely.',
+                 'You may rely on it.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+
+
+
+
+
+
+
+
+
+
 
 client.run('NzMzODQ4OTI5MDUzMTgwMDE1.XxJQcQ.03F4iDX4CW1AUgXaU_8E3RIQ-Oo')

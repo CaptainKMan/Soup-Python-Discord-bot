11ee817d12e27cf2d0d24e5311047e406400adba
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-18 00:08:33 -0400

    Update Soups_v2.0.py

--- Soups_v2.0.py
+++ Soups_v2.0.py
@@ -3,9 +3,16 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix = '#')
 
-
 @client.event
 async def on_ready():
     print('Soups_v2.0 is ready!')
+
+"""@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left a server.')"""
 
 client.run('NzMzODQ4OTI5MDUzMTgwMDE1.XxJQcQ.03F4iDX4CW1AUgXaU_8E3RIQ-Oo')

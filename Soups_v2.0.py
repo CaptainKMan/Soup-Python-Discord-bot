ae970b51c731161022cd19b8dfd40516e9f60097
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-20 16:19:39 -0400

    Update Soups_v2.0.py

--- Soups_v2.0.py
+++ Soups_v2.0.py
@@ -11,7 +11,7 @@ client = commands.Bot(command_prefix = '#')
 # Sends ready message to console & Status
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Playing('Rainbow Six: Siege'))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('Rainbow Six: Siege'))
     print('Soups_v2.0 is ready!')
 
 #Sends join message to console

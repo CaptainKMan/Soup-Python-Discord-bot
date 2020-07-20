37e92c394e98f73efabea4887fc041c1191c1e3b
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-20 14:53:37 -0400

    Update Soups_v2.0.py

--- Soups_v2.0.py
+++ Soups_v2.0.py
@@ -18,7 +18,7 @@ client = commands.Bot(command_prefix = '#')
 # Sends ready message to console & Status
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Watching('MemeSoup.'))
+    await client.change_presence(status=discord.Status.online, activity=discord.Playing('Rainbow Six: Siege'))
     print('Soups_v2.0 is ready!')
 
 #Sends join message to console

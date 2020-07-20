9814ea4024883becc2d174782690490bb1f67f7b
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-20 11:03:06 -0400

    Update Soups_v2.0.py

--- Soups_v2.0.py
+++ Soups_v2.0.py
@@ -7,20 +7,6 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix = '$')
 
-#load commands from files
-@client.command()
-async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
-
-#unload commands
-@client.command()
-async def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
-
 @client.event
 async def on_ready():
     print('Soups_v2.0 is ready!')

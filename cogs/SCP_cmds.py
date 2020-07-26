7f9f718cb70aad627f9ec30576647c399ef44844
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-25 22:23:12 -0400

    Update SCP_cmds.py

--- cogs/SCP_cmds.py
+++ cogs/SCP_cmds.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+client = commands.Bot(command_prefix = '*')
+
 class SCP_cmds(commands.Cog):
     def __init__(self, client):
         self.client = client

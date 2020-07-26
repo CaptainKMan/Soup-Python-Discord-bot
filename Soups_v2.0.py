65a2512376b719ba0a784288cc3ac96351d0768f
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-25 22:18:32 -0400

    Update Soups_v2.0.py

--- Soups_v2.0.py
+++ Soups_v2.0.py
@@ -9,11 +9,11 @@ client = commands.Bot(command_prefix = '*')
 
 
 
-@client.comman()
+@client.command()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
 
-@client.comman()
+@client.commands()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 

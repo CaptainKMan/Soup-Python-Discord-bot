24a3410b2f8078ff68019d49eb8bdd89a33fcdc7
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-28 19:54:59 -0400

    Update Help_cmd.py

--- cogs/Help_cmd.py
+++ cogs/Help_cmd.py
@@ -18,13 +18,13 @@ class Help_cmd(commands.Cog):
         author = ctx.message.author
 
         embed = discord.Embed(
-            color = 0x0099ff,
-            title='These are the commands:',
 
+            color = 0x0099ff,
+            title = 'Soup v2.1 Bot Commands',
+            description = 'These are the commands:'
 
         )
-
-        embed.set_author(name="Soup v2.1 Bot Commands", url="https://raw.githubusercontent.com/CaptainKMan/Readme/master/README.md", icon_url="https://cdn.discordapp.com/avatars/733848929053180015/c3f5b24b94a47c8f079c65e740d92a0c.png?size=128")
+        
         embed.add_field(name="The Ping Command", value="*ping = pong", inline=False)
         embed.add_field(name="Youtube", value="*youtube = sends the Soup Kitchen's youtube link", inline=False)
         embed.add_field(name="8ball", value="*8ball = an 8ball", inline=False)

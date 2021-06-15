36e1a7772da3f472f9b47f9037faae569ce1e5f3
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2021-06-15 09:36:05 -0400

    Update Soups_v2.0.py

--- Soups_v2.0.py
+++ Soups_v2.0.py
@@ -10,29 +10,30 @@ client = commands.Bot(command_prefix = '*')
 
 client.remove_command('help')
 
-#limit Testing CMDs to me ( Captain_KMan#8603 <id: 357663989418688513> )
+#start limit testing cmds to me ( Captain_KMan#8827 <id: 357663989418688513> )
 @client.event
-async def on_message(message):
+async def on_message(message, ctx, extension):
     if type(message.author) == discord.user.ClientUser:357663989418688513
-async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
 
 @client.event
-async def on_message(message):
+async def on_message(message, ctx):
     if type(message.author) == discord.user.ClientUser:357663989418688513
-async def no(ctx):
     await ctx.send('Currently only <@357663989418688513> has access')
+#end limiting cmds to me
 
 
-
+#loads cogs
 @client.command()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
 
+#unloads cogs
 @client.command()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
+#filepath to cogs
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
@@ -58,5 +59,5 @@ async def on_member_remove(member):
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
-
+#lets the bot run
 client.run(os.environ.get('BOT_TOKEN'))

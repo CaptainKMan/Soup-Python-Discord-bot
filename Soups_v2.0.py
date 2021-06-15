fa1ee30d7ba8837de62f0ce4ab6b3c14f0def0fa
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2021-06-15 09:26:01 -0400

    Update Soups_v2.0.py

--- Soups_v2.0.py
+++ Soups_v2.0.py
@@ -11,7 +11,7 @@ client = commands.Bot(command_prefix = '*')
 client.remove_command('help')
 
 #limit Testing CMDs to me ( Captain_KMan#8603 <id: 357663989418688513> )
-"""@client.event
+@client.event
 async def on_message(message):
     if type(message.author) == discord.user.ClientUser:357663989418688513
 async def load(ctx, extension):
@@ -21,7 +21,7 @@ async def load(ctx, extension):
 async def on_message(message):
     if type(message.author) == discord.user.ClientUser:357663989418688513
 async def no(ctx):
-    await ctx.send('Currently only <@357663989418688513> has access')"""
+    await ctx.send('Currently only <@357663989418688513> has access')
 
 
 

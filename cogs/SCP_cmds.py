0ea08bbde3db6bcfb89f4b667cec0a941ce52752
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2021-06-16 11:04:40 -0400

    Update SCP_cmds.py

--- cogs/SCP_cmds.py
+++ cogs/SCP_cmds.py
@@ -13,21 +13,21 @@ class SCP_cmds(commands.Cog):
         self.client = client
 
     # SCP command
-    '''@client.command()
+    @client.command()
     async def scp(self, ctx):
         scp = random.randint(1, 5000)
         if scp < (10):
             await ctx.send(f'SCP-000{scp}')
             await ctx.send(f'http://www.scp-wiki.net/scp-000{scp}')
-        if scp > (10):
-            await ctx.send(f'SCP-00{scp}')
-            await ctx.send(f'http://www.scp-wiki.net/scp-00{scp}')
         if scp < (100) and scp >= (10):
             await ctx.send(f'SCP-00{scp}')
             await ctx.send(f'http://www.scp-wiki.net/scp-00{scp}')
-        if scp > (100):
+        if scp > (100) and scp < (1000):
             await ctx.send(f'SCP-0{scp}')
-            await ctx.send(f'http://www.scp-wiki.net/scp-0{scp}')'''
+            await ctx.send(f'http://www.scp-wiki.net/scp-0{scp}')
+        if scp >= (1000):
+            await ctx.send(f'SCP-{scp}')
+            await ctx.send(f'http://www.scp-wiki.net/scp-{scp}')
 
     # SCP link for specified SCP
     @client.command()

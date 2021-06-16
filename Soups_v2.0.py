ad2c28973fc9185a6d60119b8f9904c0c399c7e8
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2021-06-16 14:18:34 -0400

    Update Soups_v2.0.py

--- Soups_v2.0.py
+++ Soups_v2.0.py
@@ -9,13 +9,13 @@ from discord.ext import commands, tasks
 from discord.ext.commands import errors
 
 #Load prefix stuff (doesnt work with heroku)
-'''
+"""
 def get_prefix(client, message):
     with open('prefixes.json', 'r') as f:
         prefixes = json.load(f)
     
     return prefixes[str(message.guild.id)]
-'''
+"""
 
 client = commands.Bot(command_prefix = '*')
 status = cycle(['DARK SOULS III', 'Can I interest you in everything? All of the time?','Rainbow Six: Quarantine', 'Elden Ring', """with Joseph's internet""", 'Breath of the Wild 2', 'with my feelings', 'with life itself', 'Bo Burnham: Welcome to the internet'])
@@ -42,7 +42,7 @@ async def change_status():
     await client.change_presence(activity=discord.Game(next(status)))
 
 #on joining a guild writes down prefix for that guild (doesnt work with heroku)
-'''
+"""
 @client.event
 async def on_guild_join(guild):
     with open('prefixes.json', 'r') as f:
@@ -52,10 +52,10 @@ async def on_guild_join(guild):
 
     with open('prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
-'''
+"""
 
 #on leaving guild deletes prefix entry (doesnt work with heroku)
-'''
+"""
 @client.event
 async def on_guild_remove(guild):
     with open('prefixes.json', 'r') as f:
@@ -65,7 +65,7 @@ async def on_guild_remove(guild):
 
     with open('prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
-'''
+"""
 
 # Sends ready message to console & Status
 @client.event

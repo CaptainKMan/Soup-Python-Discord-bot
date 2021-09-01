ecafdc13b2d432c2ceb16c5db74783db68210a05
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2021-09-01 15:14:32 -0400

    Update Soups_v2.0.py

--- Soups_v2.0.py
+++ Soups_v2.0.py
@@ -20,6 +20,10 @@ def get_prefix(client, message):
 client = commands.Bot(command_prefix = '*')
 status = cycle(['DARK SOULS III', 'Can I interest you in everything? All of the time?','Rainbow Six: Quarantine', 'Elden Ring', """with Joseph's internet""", 'Breath of the Wild 2', 'with my feelings', 'with life itself', 'Bo Burnham: Welcome to the internet'])
 
+#class CustomHelpCommand(commands.HelpCommand):
+#
+#    def __init__(self):
+#        super().__init__() 
 
 #loads cogs
 @client.command()

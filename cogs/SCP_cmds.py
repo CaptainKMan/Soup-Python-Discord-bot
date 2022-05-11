c4e3a8c8068f942371027e0ce11ea82daebeaa1f
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2022-05-11 09:14:37 -0400

    Update SCP_cmds.py

--- cogs/SCP_cmds.py
+++ cogs/SCP_cmds.py
@@ -1,9 +1,5 @@
-import discord
 import random
-import os
-import time
-from itertools import cycle
-from discord.ext import commands, tasks
+from discord.ext import commands
 
 client = commands.Bot(command_prefix='*')
 
